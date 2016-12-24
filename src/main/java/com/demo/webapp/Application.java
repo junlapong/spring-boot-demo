c366a41f88efbd63802a6a3ab392957daa7acb8e
Junlapong L <junlapong@gmail.com>
2016-12-24 08:21:48 +0700

    spring boot 1.4.3

--- src/main/java/com/demo/webapp/Application.java
+++ src/main/java/com/demo/webapp/Application.java
@@ -2,25 +2,24 @@ package com.demo.webapp;
 
 import com.demo.webapp.domain.Post;
 import com.demo.webapp.domain.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    private PostRepository repository;
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
-    public void run(String... strings) throws Exception {
-        for (int i = 1; i <= 10; i++) {
-            repository.save(new Post("My post number #" + (i)));
-        }
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        logger.info("*** Starting Servlet ***");
+        return application.sources(Application.class);
     }
+
 }
